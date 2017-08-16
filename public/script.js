e365ae9be586b00c4ddc61486a54007217336f65
Derek Moon <dmoon79@gmail.com>
2017-08-16 03:34:57 -0700

    through lesson 21

--- public/script.js
+++ public/script.js
@@ -1,1 +1,18 @@
-console.log('It works.');
+new Vue({
+  el: '#app',
+  data: {
+    total: 0,
+    items: [
+      { title: 'Item 1' },
+      { title: 'Item 2' },
+      { title: 'Item 3' },
+    ],
+    cart: []
+  },
+  methods: {
+    addItem: function(index){
+      this.total += 9.99;
+      this.cart.push(this.items[index]);
+    }
+  }
+});
